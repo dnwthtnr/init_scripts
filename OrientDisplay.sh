6e410b808727d59ced5195f6395931fd38d6bc33
dnwthtnr <dunworthtanner@gmail.com>
2024-03-14 12:07:58 -0400

    Update OrientDisplay.sh
    (From phone): separated monitor orientation update into func

--- OrientDisplay.sh
+++ OrientDisplay.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-orientations = [right, left]
 
+setMonitorOrientation(){
+    targetOutput = $1
+    desiredOrientation = $2
+    echo "Call made to set monitor orientation for output: $1 to rotation: $2"
+    
+    if [desiredOrientation == default]
+        then
+        return 0
+    
+    xrandr --output targetOutput --rotate desiredOrientation
+    return 0
+}
+
+
+orientations = [right, left, default]
 display = HDMI-0
 
-desiredOrientation = orientations[0]
+setMonitorOrientation display orientation[0]
 
-xrandr --output display --rotate desiredOrientation
